f31f0fdc2bcc355467edf5e8872324aa040c4379
Pleyer99Bit <zaffa2393@gmail.com>
2021-12-23 09:42:46 +0100

    Aggiustamento Random

--- src/App.java
+++ src/App.java
@@ -8,9 +8,9 @@ public class App {
         r[2] ="TestRisposta";
         c.Crea("TestDomanda1", r, "TestRispGiust", 1);
         String[] r1 = new String[3];
-        r[0] ="TestRisposta";
-        r[1] ="TestRisposta";
-        r[2] ="TestRisposta";
+        r1[0] ="TestRisposta";
+        r1[1] ="TestRisposta";
+        r1[2] ="TestRisposta";
         c.Crea("TestDomanda2", r1, "TestRispGiust", 1);
         Quesito q = new Quesito();
         q = c.Random();
